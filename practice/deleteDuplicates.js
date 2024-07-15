337cc2510c3c009ba0743115fc7f69d3d3965072
sujeetghodke <sujeetghodke16@gmail.com>
2024-07-15 23:24:27 +0530

    1 problem added

---
+++ practice/deleteDuplicates.js
@@ -0,0 +1,13 @@
+var deleteDuplicates = function (head) {
+  let cur = head;
+  while (cur && cur.next) {
+    if (cur.val == cur.next.val) {
+      cur.next = cur.next.next;
+    } else {
+      cur = cur.next;
+    }
+  }
+  return head;
+};
+
+console.log(deleteDuplicates(1, 1, 2, 3, 3));
